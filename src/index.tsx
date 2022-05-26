dc10470accaedc2259e8f68a4d36ce850aa29ce2
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-05-26 18:38:32 +0900

    Theme with TS

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
-
-const darkMode = {
-  textColor: "whitesomke",
-  backgroundColor: "#111",
-};
+import { lightTheme, darkTheme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkMode}>
+    <ThemeProvider theme={darkTheme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
