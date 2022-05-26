0fbc0423ebcfe6dcce29d8f9350109555ee7f623
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-05-26 18:25:22 +0900

    Form event with React and TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
+import React, { useState } from "react";
 import styled from "styled-components";
 import Circle from "./Circle";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Hello", value);
+  };
+
   return (
     <div>
-      <Circle bgColor="teal" borderColor="tomato"></Circle>
-      <Circle bgColor="tomato" borderColor="teal"></Circle>
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
